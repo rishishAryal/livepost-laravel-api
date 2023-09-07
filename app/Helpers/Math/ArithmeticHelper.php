0b4cf6dfc16dd07475762d059ed9558bacb5724b
rishisAryal <risharyal5@gmail.com>
2023-09-07 19:54:25 +0545

    tests

---
+++ app/Helpers/Math/ArithmeticHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Helpers\Math;
+
+use Throwable;
+
+class ArithmeticHelper
+{
+    public static function add(...$nums)
+    {
+        if(sizeof($nums) < 1){
+            throw new \InvalidArgumentException("Must have at least 1 argument.");
+        }
+        $sum = 0;
+        foreach ($nums as $num){
+            if ( !self::isNumeric($num)){
+                throw new \InvalidArgumentException("Argument can only be numeric.");
+            }
+            $sum += $num;
+        }
+        return $sum;
+    }
+
+    private static function isNumeric($number)
+    {
+        return (is_float($number) || is_int($number));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public static function minus(...$nums)
+    {
+        if(sizeof($nums) < 1){
+            throw new \InvalidArgumentException("Must have at least 1 argument.");
+        }
+
+        $subtraction = $nums[0];
+
+        throw_if(!self::isNumeric($subtraction), \InvalidArgumentException::class);
+
+        foreach (array_slice($nums, 1) as $num){
+            if ( !self::isNumeric($num) ){
+                throw new \InvalidArgumentException("Argument can only be numeric.");
+            }
+            $subtraction -= $num;
+        }
+        return $subtraction;
+    }
+}
---
+++ tests/Unit/ArithmeticHelper/ArithmeticHelperMinusTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit\ArithmeticHelper;
+
+use PHPUnit\Framework\TestCase;
+
+class ArithmeticHelperMinusTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+   public function test_add_can_sum_numbers_up(){
+        $num1 = 5;
+        $num2 = 10;
+
+   }
+    public function test_add_can_take_in_multiple_numbers(){
+
+    }
+    public function test_add_can_only_take_least_one_argument(){
+
+    }
+}
